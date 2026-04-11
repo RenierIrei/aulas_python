4fa071fcd68a2472bd11548f84e2f411486158e4
Frances Irei <renierirei@yahoo.com.br>
2026-04-12 07:05:52 +0900

    Terminando as variáveis simples

--- aula_01/variaveis.py
+++ aula_01/variaveis.py
@@ -1,28 +1,19 @@
-
-nome = "Pedro"
-idade = 25
-print(f"Olá {nome}, ano que vem você fará {idade+1}.")
-
-
+"""
+    Essas são as variáveis simples comumente usadas, têm mais, mas
+    sem necessidades no momento.
+"""
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+nome = "Pedro"  # str   → texto
+altura = 1.78   # int   → número inteiro
+idade = 25  # float → número decimal
+adulto = True   # bool  → verdadeiro/falso
+frutas = ["banana", "maçã", "ameixa", "manga"]  # list  → lista ordenada, permite duplicatas, mutável
+posicoes_geograficas = ("latitude", "longitude")    # tuple → lista ordenada, permite duplicatas, imutável
+capitais = {
+    "Brasil": "Brasilia",
+    "Argentina": "Buenos Aires",
+    "Japão": "Tóquio",
+}   # dict  → chave → valor
+cartas_pokemon = {"Pikachu", "Kabigon", "Nyaoha", "Eevy", "Kabigon", "Kamonegi", }  # set   → coleção sem ordem, sem duplicatas
 
